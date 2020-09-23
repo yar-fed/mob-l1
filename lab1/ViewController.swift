b6548fdcc3fa0769ab2de26028ece2e43bb395b6
yaroslav <ghostfighter183@gmail.com>
2020-09-23 15:21:04 +0300

    Initial Commit

---
+++ lab1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lab1
+//
+//  Created by Ivan Feofanov on 20.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
